5d162195ad01a1aa6adf8b8f32364bf14a869758
Abdallah-Bola7 <abda84784@gmail.com>
2024-10-13 23:31:14 +0300

    a simple calculater

---
+++ Calculater/src/calculater/Calculater.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculater;
+
+/**
+ *
+ * @author AhmeD1907
+ */
+
+
+import java.util.Scanner;
+
+public class Calculater {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operator;
+
+        System.out.println("Welcome to the Simple Calculator!");
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.printf("%.2f + %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.printf("%.2f - %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.printf("%.2f * %.2f = %.2f%n", num1, num2, result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.printf("%.2f / %.2f = %.2f%n", num1, num2, result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
+
+
